d8bf94e55a49861a2d33c25c25f5d711b97102b9
MilanAarick <125361363+El-fonzo@users.noreply.github.com>
2023-05-04 22:13:09 +0100

    Update Hero1.jsx

--- src/component/Hero1/Hero1.jsx
+++ src/component/Hero1/Hero1.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
         <div className="container hero__inner-cont">
         
                 
-                    <div className="  hero__inner-cont__left data-aos= zoom-in ">
+                    <div className="  hero__inner-cont__left" data-aos="zoom-out-right" data-aos-duration="1000">
                         <p className='heading'>A Journey to the moon</p>
                         <h1 className='hero-heading'>Welcome to <span>LIGMA</span></h1>
                         <p className="">$LIGMA is an erc20 0 tax token based on the neo-famous ligma balls meme. Built and backed by a seasoned team and a well connected influencer network, we aim to ligma balls. 
@@ -42,12 +42,12 @@ const Hero = () => {
                              <a href="https://twitter.com/ligmaerc" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="twitter" /></a>
                             <a href="  https://etherscan.io/token/0x8ec891a6f3897700de73c01d4ffe7f34f0330b66#code" target="_blank" rel="noopener noreferrer"><img src={etherscan} alt="etherscan" /></a>
                              <a href="https://t.me/ligmacoin" target="_blank" rel="noopener noreferrer"><img src={telegram} alt="telegra" /></a>
-                             <a href="https://app.uniswap.org/#/swap" target="_blank" rel="noopener noreferrer"><img src={uniswap} alt="uniswap" /></a>
+                             <a href="https://app.uniswap.org/#/swap?outputCurrency=0x8ec891a6f3897700de73c01d4ffe7f34f0330b66" target="_blank" rel="noopener noreferrer"><img src={uniswap} alt="uniswap" /></a>
                              <a href="https://www.dextools.io/app/en/ether/pair-explorer/0xa9671c88c60ecfda5d87176cee868867e84a0ad3" target="_blank" rel="noopener noreferrer"><img src={dextool} alt="dextool" /></a>
                         </div>
   </div>
                   
-                    <div className="hero-img">
+                    <div className="hero-img" data-aos="zoom-out-left" data-aos-duration="1000">
                        
                     <img src= {lig} alt="hero picture"  className="image"/>
                     </div>
