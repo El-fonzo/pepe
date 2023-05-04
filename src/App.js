22d35349c97ffbb185e79f13dfaad5238c1a16c8
MilanAarick <125361363+El-fonzo@users.noreply.github.com>
2023-05-04 02:19:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Navbar from './component/Navbar/Navbar/Navbar';
 
 import './App.css';
 import Hero from './component/Hero1/Hero1'
-import About from './component/About/About';
-import Roadmap from './component/Roadmap/Roadmap';
-import Tokenomics from './component/Tokenomics/Tokenomics';
+// import About from './component/About/About';
+// import Roadmap from './component/Roadmap/Roadmap';
+// import Tokenomics from './component/Tokenomics/Tokenomics';
 import Footer from './component/Footer/Footer';
 import Abtt from './component/Abtt/Abtt';
 
